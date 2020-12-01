1f72cfe6aa98b8db8dd21f1a06be79a8a9cd3c4f
Quentin <quentin1.thomas@epitech.eu>
2020-12-01 11:37:00 +0100

    init

---
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,33 @@
+/*
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ A view showing a list of landmarks.
+ */
+
+import SwiftUI
+import SwiftUICharts
+
+struct LandmarkList: View {
+    var body: some View {
+        
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LineChartView(data: landmark.graphs.value, title: landmark.name, legend: landmark.price)
+//                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Cryptos"))
+        }
+        
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+        
+    }
+    
+}
