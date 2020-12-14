0bdc560ffa2b0d4295691243000ce4afd98554d4
Quentin <quentin1.thomas@epitech.eu>
2020-12-14 15:54:58 +0100

    details view modification

--- Landmarks/Landmarks/LandmarkList.swift
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -47,17 +47,14 @@ import SwiftUICharts
 
 struct LandmarkList: View {
     var body: some View {
-        
-            VStack {
-                List(landmarkData) { landmark in
-                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                        LandmarkRow(landmark: landmark)
-                    }
+        VStack (alignment: .leading) {
+            Text("Cryptos").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            List(landmarkData){ landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
                 }
-            }
-            .navigationBarTitle(Text("Cryptos"))
-            
-        
+            }.navigationTitle(Text("blbl"))
+        }
     }
 }
 
